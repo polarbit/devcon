b3c3a5ed9075f3ba6eba353d01d4e310bfb46e11
Şafak Ulusoy <safakulusoy@gmail.com>
2021-08-26 03:32:23 +0300

    Trivial fix in nvim config path

--- run-container.sh
+++ run-container.sh
@@ -6,7 +6,7 @@ DEVCON_SSH_AUTH_KEY=$(cat $(find ~/.ssh/id*.pub))
 
 docker run -d \
 	--name devcon \
-	--mount type=bind,source="$(pwd)"/init.vim,target=/home/dev/.config/init.vim \
+	--mount type=bind,source="$(pwd)"/init.vim,target=/home/dev/.config/nvim/init.vim \
 	--mount type=bind,source="$(pwd)"/container-entrypoint.sh,target=/home/dev/container-entrypoint.sh \
 	--mount source=devcon-workspace,target=/home/dev/workspace \
 	-p 22:22 \
